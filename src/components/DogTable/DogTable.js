ff9eef90dc3b5b700fdb5b39d60d435ed7d426f2
reazul7 <islamreazul7777@gmail.com>
2021-09-22 21:14:56 +0600

    table redesign

--- src/components/DogTable/DogTable.js
+++ src/components/DogTable/DogTable.js
@@ -1,53 +1,59 @@
 import React from "react";
 
 const DogTable = (props) => {
-    const {handleRemoveLike, handleRemoveDisLike, dog, likeDog, disLikeDog} = props;
+  const { handleRemoveLike, handleRemoveDisLike, dog, likeDog, disLikeDog } =
+    props;
 
   return (
-    <div>
+    <div className="container">
       <table className="table text-center">
-        <thead>
+        <thead className="border-1 border-black">
           <tr>
-            <th className="text-green-600">Like</th>
-            <th className="text-red-600">Dislike</th>
+            <th className="border-1 border-green-600 text-green-600">Like</th>
+            <th className="border-1 border-red-600 text-red-600">Dislike</th>
           </tr>
         </thead>
-        
+
         <tbody>
           <tr>
-            <td>
+            <td className="border-1 border-green-600 w-60">
               {likeDog ? (
-                <div>
+                <div className="justify-center mx-auto">
                   <img
                     src={dog.message}
                     alt=""
-                    style={{ height: "100px", width: "100px" }}
+                    className="w-32 h-32 mx-auto pb-2 rounded shadow"
                   />
-                  <br />
-                  <button onClick={handleRemoveLike}>Remove from Like</button>
-                </div>
-              ) : (
-                ""
-              )}{" "}
-            </td>
-
-            <td>
-              {disLikeDog ? (
-                <div>
-                  <img
-                    src={dog.message}
-                    alt=""
-                    style={{ height: "100px", width: "100px" }}
-                  />
-                  <br />
-                  <button onClick={handleRemoveDisLike}>
-                    Remove from Dislike
+                  <button
+                    className="border-1 border-green-500 bg-gray-200 px-4 py-1 rounded text-green-600"
+                    onClick={handleRemoveLike}
+                  >
+                    Removed from Like
                   </button>
                 </div>
               ) : (
-                ""
+                <p className="font-bold text-gray-400">Empty</p>
               )}{" "}
-              <br />
+            </td>
+
+            <td className="border-1 border-red-600 w-60">
+              {disLikeDog ? (
+                <div className="justify-center mx-auto">
+                  <img
+                    src={dog.message}
+                    alt=""
+                    className="w-32 h-32 mx-auto pb-2 rounded shadow"
+                  />
+                  <button
+                    className="border-1 border-red-500 bg-gray-200 px-4 py-1 rounded text-red-600"
+                    onClick={handleRemoveDisLike}
+                  >
+                    Removed from Dislike
+                  </button>
+                </div>
+              ) : (
+                <p className="font-bold text-gray-400">Empty</p>
+              )}{" "}
             </td>
           </tr>
         </tbody>
